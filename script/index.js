7f0b42d5ee16865adf09a72adea7980ac56f95c2
weaknumbness <seregastepanov05@mail.ru>
2025-10-09 14:50:31 +0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,13 +1,13 @@
-const rootOfEd = getComputedStyle(document.documentElement);
-const mainColor = rootOfEd.getPropertyValue("--main-color").trim();
-const colorOfPage = document.getElementById("colorPicker");
-colorOfPage.value = mainColor;
-const checkBtn = document.getElementById("check");
-checkBtn.addEventListener("click", () => {
-  const selectedColor = colorOfPage.value;
-  document.documentElement.style.setProperty("--main-color", colorOfPage.value);
-  localStorage.setItem("savedColor", selectedColor);
-});
+// const rootOfEd = getComputedStyle(document.documentElement);
+// const mainColor = rootOfEd.getPropertyValue("--main-color").trim();
+// const colorOfPage = document.getElementById("colorPicker");
+// colorOfPage.value = mainColor;
+// const checkBtn = document.getElementById("check");
+// checkBtn.addEventListener("click", () => {
+//   const selectedColor = colorOfPage.value;
+//   document.documentElement.style.setProperty("--main-color", colorOfPage.value);
+//   localStorage.setItem("savedColor", selectedColor);
+// });
 
 function animateAndGoTo(url) {
   document.body.classList.add("transition-out");
@@ -33,19 +33,19 @@ arrowForMenu.addEventListener("click", (e) => {
 
 const mainSection = document.getElementById("contentSection");
 const colorForBack = localStorage.getItem("savedColor");
-mainSection.style.setProperty("background", colorForBack);
+// mainSection.style.setProperty("background", colorForBack);
 
 window.addEventListener("DOMContentLoaded", () => {
   const localSavedColor = localStorage.getItem("savedColor");
   document.documentElement.style.setProperty("--main-color", localSavedColor);
-  if (localSavedColor === null) {
-    colorOfPage.value = "#4973ff";
-    document.documentElement.style.setProperty("--main-color", "#4973ff");
-  } else {
-    colorOfPage.value = localSavedColor;
-  }
+  // if (localSavedColor === null) {
+  //   colorOfPage.value = "#4973ff";
+  //   document.documentElement.style.setProperty("--main-color", "#4973ff");
+  // } else {
+  //   colorOfPage.value = localSavedColor;
+  // }
   document.body.classList.add("loaded");
-  const contentOfPage = document.querySelector(".content");
-  contentOfPage.classList.add("loadedCon");
-  console.log(colorOfPage.value);
+  // const contentOfPage = document.querySelector(".content");
+  // contentOfPage.classList.add("loadedCon");
+  // console.log(colorOfPage.value);
 });
